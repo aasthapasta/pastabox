a0514e509f6ee65b94ae99a400ef3a9d22226dfc
aasthapasta <89803279+aasthapasta@users.noreply.github.com>
2021-08-30 14:04:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var pastabox ;
+function setup() {
+  createCanvas(915,901);
+
+
+  pastabox=createSprite(100,100,30,30)
+}
+
+function draw() 
+{
+  background("red");
+
+  
+drawSprites();
+}
+
+
+
+
